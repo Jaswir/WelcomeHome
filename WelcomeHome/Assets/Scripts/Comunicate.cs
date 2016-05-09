a1c4c3d546a45d0c22cf3819980a8aa7059ef9b6
Boudewijn Witteveen <516708@student.glu.nl>
2016-05-09 17:47:11 +0200

    Default

---
+++ WelcomeHome/Assets/Scripts/Comunicate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Comunicate : MonoBehaviour {
+	
+	void OnMouseDown()
+    {
+        Controller  =
+        Bullet bulletscript = b.GetComponent<Bullet>();
+    }
+}
---
+++ WelcomeHome/Assets/Scripts/LinkObjects.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LinkObjects : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject m_FirstSlot;
+    [SerializeField]
+    private GameObject m_SecondSlot;
+    [SerializeField]
+    private GameObject[] m_AllInteractableObjects = new GameObject[3];
+
+	void Start()
+    {
+        for (int i = 0; i < m_AllInteractableObjects.Length; i++)
+        {
+            m_AllInteractableObjects[i] = GameObject.Find("Cube" + i);
+        }
+	}
+	
+
+	void OnClick()
+    {
+	
+	}
+}
