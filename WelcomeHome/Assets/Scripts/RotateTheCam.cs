19cace2aaaae422bd7bd7ec599e514df3e6adbe6
Boudewijn Witteveen <516708@student.glu.nl>
2016-05-09 16:56:33 +0200

    Revert "Camera Patch"
    This reverts commit 37ce25a44a1710dfbee7ce54418305a7b70d49fa.

---
+++ WelcomeHome/Assets/Scripts/RotateTheCam.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateTheCam : MonoBehaviour {
+	
+
+	void Update()
+    {
+        Vector3 Rotation = new Vector3(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), 0);
+
+        //Rotation.x = Mathf.Clamp(Rotation.y, -44.89f, 50);
+
+        transform.Rotate(Rotation);
+	}
+}
