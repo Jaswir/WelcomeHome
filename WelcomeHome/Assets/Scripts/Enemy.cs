9e13f4bc1efe4913a8f60915d8e52a841ac453c5
Jaswir <j.t.raghoe@students.uu.nl>
2016-05-09 22:30:28 +0200

    k

---
+++ WelcomeHome/Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum enemyState {
+    Idle = 0,
+    Walking
+}
+
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField]
+    private Animator m_Animator;
+    public enemyState m_EnemyState = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        m_Animator.SetInteger("EnemyState", (int)m_EnemyState);
+
+    }
+}
---
+++ WelcomeHome/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+    GameObject treasure;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        //Find Treasure object
+        treasure = GameObject.FindGameObjectWithTag("Treasure");
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Match Direction and Align with treasure
+        Vector3 dir = treasure.transform.position - gameObject.transform.position;
+        Vector3 rotation = gameObject.transform.localRotation.eulerAngles - treasure.transform.localRotation.eulerAngles;
+
+        Enemy EnemyScript = gameObject.GetComponent<Enemy>();
+        EnemyScript.m_EnemyState = enemyState.Walking;
+        gameObject.transform.position += dir * speed;
+       // gameObject.transform.rotation = Quaternion.Euler(rotation);
+
+
+    }
+}
