66c187b8d6a2034e1c955117983616cbdca72233
Boudewijn Witteveen <516708@student.glu.nl>
2016-05-09 12:17:47 +0200

    TESTING

---
+++ WelcomeHome/Assets/RotateTheCam.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateTheCam : MonoBehaviour {
+	
+
+	void Update()
+    {
+        transform.Rotate(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), 0);
+	}
+}
